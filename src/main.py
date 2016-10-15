4a0e6a63e7e5b8e16b412c13ac14b3f47da459e6
Hugo Lundin <hugo@hugolundin.se>
2016-10-15 22:35:01 +0200

    Added a third condition for mirroring folder, and secured paths by using os.path.realpath()

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-import click
-import os.path
-
 from copy import copy
 from scan import Scan
+import click
+import os.path
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,18 +13,20 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 def main(base, mirror):
 
     # Prevent that mirroring is done to the base folder
-    if base == mirror:
+    if os.path.realpath(base) == os.path.realpath(mirror):
         print('Error: The directories can not be the same.')
         return
 
+    if os.path.realpath(base) in os.path.realpath(mirror):
+        print('Error: Mirror folder can not be a sub-directory of base folder.')
+        return
+
     # If the mirror folder already exists, ask if the user wants to overwrite it
-    if os.path.exists(mirror):
+    if os.path.exists(os.path.realpath(mirror)):
         i = input('Do you want to overwrite {mirror}? (Y/n) '.format(mirror=mirror))
         if i.lower() not in ["", "y", "yes"]:
             print('Aborting...')
             return
-
-    
 
     # Initial mirroring before starting scan
     print(copy(base, mirror))
@@ -34,6 +35,3 @@ def main(base, mirror):
 
 if __name__ == '__main__':
     main()
-
-
-
