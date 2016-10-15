c841e22ad99a8c80d4ddd99a160f152495934837
Hugo Lundin <hugo@hugolundin.se>
2016-10-15 22:10:13 +0200

    Reworded mirroring event.

--- src/copy.py
+++ src/copy.py
@@ -2,5 +2,5 @@ from distutils.dir_util import copy_tree
 
 def copy(base, mirror):
     copy_tree(base, mirror, update=1)
-    return ("Copied '{base}' to '{mirror}'".format(base=base,
-                                                   mirror=mirror))
+    return ("Mirroring '{base}' to '{mirror}'.".format(base=base,
+                                                       mirror=mirror))
