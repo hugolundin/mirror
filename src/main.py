f8f33f550d09686ec49b85101a3c9900c0b62e61
Hugo Lundin <hugo@hugolundin.se>
2016-10-15 22:12:04 +0200

    Added a second condition. Also added some comments.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-import os.path
-from scan import Scan
-from copy import copy
 import click
+import os.path
+
+from copy import copy
+from scan import Scan
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,9 +13,19 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 @click.argument('mirror', type=click.Path())
 def main(base, mirror):
 
+    # Prevent that mirroring is done to the base folder
     if base == mirror:
         print('Error: The directories can not be the same.')
         return
+
+    # If the mirror folder already exists, ask if the user wants to overwrite it
+    if os.path.exists(mirror):
+        i = input('Do you want to overwrite {mirror}? (Y/n) '.format(mirror=mirror))
+        if i.lower() not in ["", "y", "yes"]:
+            print('Aborting...')
+            return
+
+    
 
     # Initial mirroring before starting scan
     print(copy(base, mirror))
